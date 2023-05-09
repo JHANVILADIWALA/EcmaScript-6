116cf7214c30df46432a7829fe33fdbec4273d7c
Jhanvi Ladiwala <105975195+JHANVILADIWALA@users.noreply.github.com>
2023-05-09 11:36:40 +0530

    Create templateliterals.js

---
+++ templateliterals.js
@@ -0,0 +1,14 @@
+ //string template literals
+//handle lengthy strings
+//pass variable => ${}
+let one="name is "
+let two="jhanvi"
+function fun(){
+  return "jhanvi ladiwala"
+}
+x=10
+y=20
+console.warn(`my ${one} ${two}`)
+console.warn(`my ${one} ${fun()} ${x+y}`)
+
+//we can combine 2 template string
